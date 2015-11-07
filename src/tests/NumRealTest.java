c55e1920f3aace3c71a6f3c2204a8154e9bdf1e6
Diego <diego.brg92@gamil.com>
2015-11-07 10:04:09 -0200

    aterado testes

--- src/tests/NumRealTest.java
+++ src/tests/NumRealTest.java
@@ -24,26 +24,17 @@ public class NumRealTest {
 	
 	@Test
 	public void lexanTest1() throws Exception {
-		Token[] tks = new Token[4];
+		Token[] tks = new Token[9];
 		tks[0] = new Token("123",TypeToken.NUM_REAL,  6, 1, 0, 2);
 		tks[1] = new Token(".1", TypeToken.NUM_REAL,  2, 1, 4, 5);
 		tks[2] = new Token(".123", TypeToken.NUM_REAL,  2, 1, 7, 10);
-		tks[3] = new Token("1.23", TypeToken.NUM_REAL,  2, 1, 12, 15);
-		
-		lexanReconheceNumReal("123 .1 .123 1.23", tks);
-//		lexanReconheceNumReal("1.23 ",  new String[]{"1.23"}, 2, 3);
-//		lexanReconheceNumReal("12.3 ",  new String[]{"12.3"}, 2, 3);
-//		lexanReconheceNumReal("12. ",  new String[]{"12."}, 7, 2);
-//		lexanReconheceNumReal("1. ",  new String[]{"1."}, 7, 1);
-//		lexanReconheceNumReal("1.4E ",  new String[]{"1.4E"}, 3, 3);
-//		lexanReconheceNumReal("14E ",  new String[]{"14E"}, 3, 2);
-//		lexanReconheceNumReal("1.4E23 ",  new String[]{"1.4E23"}, 5, 5);
-//		lexanReconheceNumReal("1.4E-2 ",  new String[]{"1.4E-2"}, 5, 5);
-//		lexanReconheceNumReal("1.4E+23 ",  new String[]{"1.4E+23"}, 5, 6);
-//		lexanReconheceNumReal("3E",  new String[]{"3E"}, 3, 1);
-//		lexanReconheceNumReal("34E3 ",  new String[]{"34E3"}, 5, 3);
-//		lexanReconheceNumReal("35E36 ",  new String[]{"35E36"}, 5, 4);
-//
+		tks[3] = new Token("190.23", TypeToken.NUM_REAL,  2, 1, 12, 17);
+		tks[4] = new Token("190.", TypeToken.NUM_REAL,  7, 1, 18, 22);
+		tks[5] = new Token("1.4E", TypeToken.NUM_REAL,  3, 1, 23, 27);
+		tks[6] = new Token("1E4", TypeToken.NUM_REAL,  5, 1, 0, 31);
+		tks[7] = new Token("1.5E-3", TypeToken.NUM_REAL,  5, 1, 0, 38);
+		tks[8] = new Token("1.5E+3", TypeToken.NUM_REAL,  5, 1, 0, 45);
+		lexanReconheceNumReal("\t123 .1 \n.123 190.23 190. 1.4E 1E4 1.5E-3 1.5E+3", tks);
 //		lexanReconheceNumReal(" \t\n \n 123 \t\n",  new String[]{"123"}, 6, 13, 3);
 		
 	}	
